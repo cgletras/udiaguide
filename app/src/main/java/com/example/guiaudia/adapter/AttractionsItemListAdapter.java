075b4e0d14be4fca912e4972266ea699e44d486b
CG Stein <cgletras@gmail.com>
2021-12-09 01:29:50 -0300

    adds maps logic. Still missing map queries
    Signed-off-by: CG Stein <cgletras@gmail.com>

--- app/src/main/java/com/example/guiaudia/adapter/AttractionsItemListAdapter.java
+++ app/src/main/java/com/example/guiaudia/adapter/AttractionsItemListAdapter.java
@@ -1,12 +1,15 @@
 package com.example.guiaudia.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -44,11 +47,25 @@ public class AttractionsItemListAdapter extends ArrayAdapter<TouristicAttraction
         TextView attractionName = listItemView.findViewById(R.id.tv_item_name);
         TextView attractionAddress = listItemView.findViewById(R.id.tv_item_address);
         ImageView attractionImage = listItemView.findViewById(R.id.iv_item_image);
+        ImageView mapIcon = listItemView.findViewById(R.id.iv_map_icon);
 
         // Set info from class into view
         attractionName.setText(currentItemAttraction.getmName());
         attractionAddress.setText(currentItemAttraction.getmAddress());
         attractionImage.setImageResource(currentItemAttraction.getmImageId());
+
+        mapIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "TESTE", Toast.LENGTH_SHORT).show();
+                Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if (mapIntent.resolveActivity(getContext().getPackageManager()) != null) {
+                    getContext().startActivity(mapIntent);
+                }
+            }
+        });
 
         return listItemView;
     }
